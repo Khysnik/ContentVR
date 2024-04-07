ecc9e1e40155719c49e19595c2c3777de8544f0f
Khysnik <pangea7707@hotmail.com>
2024-04-07 13:14:17 -0400

    first commit

---
+++ Plugin.cs
@@ -0,0 +1,111 @@
+﻿using BepInEx;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using UnityEngine.XR.Management;
+using UnityEngine.XR.OpenXR;
+using Unity.XR.OpenVR;
+using HarmonyLib;
+using Valve.VR;
+
+
+namespace ContentVR
+{
+
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.HerrFristi.VRMods.VRath";
+        public const string PLUGIN_NAME = "VRMaker";
+        public const string PLUGIN_VERSION = "0.0.2";
+
+        public static string gameExePath = Process.GetCurrentProcess().MainModule.FileName;
+        public static string gamePath = Path.GetDirectoryName(gameExePath);
+        public static string HMDModel = "";
+
+
+        public static UnityEngine.XR.Management.XRManagerSettings managerSettings = null;
+
+        public static List<UnityEngine.XR.XRDisplaySubsystemDescriptor> displaysDescs = new List<UnityEngine.XR.XRDisplaySubsystemDescriptor>();
+        public static List<UnityEngine.XR.XRDisplaySubsystem> displays = new List<UnityEngine.XR.XRDisplaySubsystem>();
+        public static UnityEngine.XR.XRDisplaySubsystem MyDisplay = null;
+
+        public static GameObject SecondEye = null;
+        public static Camera SecondCam = null;
+
+        //Create a class that actually inherits from MonoBehaviour
+        public class MyStaticMB : MonoBehaviour
+        {
+        }
+
+        //Variable reference for the class
+        public static MyStaticMB myStaticMB;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            //If the instance not exit the first time we call the static class
+            if (myStaticMB == null)
+            {
+                //Create an empty object called MyStatic
+                GameObject gameObject = new GameObject("MyStatic");
+
+
+                //Add this script to the object
+                myStaticMB = gameObject.AddComponent<MyStaticMB>();
+            }
+
+            myStaticMB.StartCoroutine(InitVRLoader());
+        }
+        public static System.Collections.IEnumerator InitVRLoader()
+        {
+
+            SteamVR_Actions.PreInitialize();
+
+            var generalSettings = ScriptableObject.CreateInstance<XRGeneralSettings>();
+            managerSettings = ScriptableObject.CreateInstance<XRManagerSettings>();
+            var xrLoader = ScriptableObject.CreateInstance<OpenVRLoader>();
+
+
+            var settings = OpenVRSettings.GetSettings();
+            settings.StereoRenderingMode = OpenVRSettings.StereoRenderingModes.MultiPass;
+
+
+            generalSettings.Manager = managerSettings;
+
+            managerSettings.loaders.Clear();
+            managerSettings.loaders.Add(xrLoader);
+
+            managerSettings.InitializeLoaderSync(); ;
+
+
+            XRGeneralSettings.AttemptInitializeXRSDKOnLoad();
+            XRGeneralSettings.AttemptStartXRSDKOnBeforeSplashScreen();
+
+            SteamVR.Initialize(true);
+
+
+            SubsystemManager.GetInstances(displays);
+            MyDisplay = displays[0];
+            MyDisplay.Start();
+
+            Logs.WriteInfo("SteamVR hmd modelnumber: " + SteamVR.instance.hmd_ModelNumber);
+            HMDModel = SteamVR.instance.hmd_ModelNumber;
+
+            //new VRInputManager();
+
+            Logs.WriteInfo("Reached end of InitVRLoader");
+
+            yield return null;
+
+        }
+    }
+}
